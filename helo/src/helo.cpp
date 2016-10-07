f727e73746084bf046cbd691a0ce1d789b5783de
Gabor Pinkova <gabor.pinkova@gmail.com>
2016-10-07 23:39:39 +0200

    First step
    Inputs added

---
+++ helo/src/helo.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : helo.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "Number 1: ";
+	cin >> a;
+	cout << "Number 2: ";
+	cin >> b;
+	return 0;
+}
